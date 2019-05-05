22567e40135a204c96ac17479970783881db57a9
James Trimarco <james.trimarco@gmail.com>
2019-05-05 15:31:47 -0400

    added more data on delays to aggregations

--- get_yearly_ops.py
+++ get_yearly_ops.py
@@ -1,3 +1,6 @@
+# ISSUES
+# The csv that's output at the end of this is horribly malformed. 
+# Can you fix it?
 
 from __future__ import print_function
 import operator
@@ -11,10 +14,13 @@ findspark.init()
 # Get pyspark
 import pyspark
 
+# tmp_spark
+
 conf = pyspark.SparkConf()
 conf.set('spark.local.dir', '/Akamai_scratch/tmp_spark/')
 conf.set('spark.executor.memory', '15g')
 conf.set('spark.driver.memory', '15g')
+#conf.setMaster('local')
 
 # Tell Spark to use all the local clusters
 sc = pyspark.SparkContext('local[*]', 'airports', conf)
@@ -27,7 +33,7 @@ sess = SparkSession(sc)
 sc.setLogLevel("ERROR")
 
 # First path points to one small csv for testing
-path1 = "/Akamai_scratch/airport_alpha/raw_bts/001.csv"
+path1 = "/Akamai_scratch/airport_alpha/raw_bts/75577152_T_ONTIME_REPORTING.csv"
 # Second path is to full data
 path2 = "/Akamai_scratch/airport_alpha/raw_bts/*.csv"
 
@@ -50,57 +56,36 @@ header = raw_data.first()
 # Remove header
 airport = raw_data.filter(lambda x: x != header) 
 
-# Remove reviews that share date, origin, destination, dep_time, dep_delay
-# Note that the entire row is the value here
-all_dupes = airport.map(lambda x: ((x[3], x[6], x[10], x[13], x[14]), x))
-# Group by row values, dropping duplicates
-airport = all_dupes.reduceByKey(lambda x, y: x) \
-                   .map(lambda x: x[1])
-
 # Check against selected airport
 def is_selected(x):
     return x == ap_select
 
-def extract_hour(x):
-  """This extracts an hour between 1 and 24 from numbers formatted like '425' or '1345'."""
-  x = str(x).strip()
-  if len(x) == 3:
-    return int(x[0])
-  elif len(x) == 4 and x[:2] != '24':
-    return int(x[:2])
-  elif len(x) < 3 or (len(x) == 4 and x[:2] == '24'):
-    return(0)
- 
-# x[0] is year
-# x[1] is month
-# x[2] is day of the month
-# x[8] is origin
-# x[13] is departure time
- 
-#get count of arrivals for every unique pair (day_of_month, hour) in the month
-arrs_per_hour = airport.filter(lambda x: x[8] == ap_select) \
-	                   .map(lambda x: ((x[0], x[1], x[2], (extract_hour(x[13]))), 1)) \
-                           .reduceByKey(lambda x, y: x+y) 
+# Remove reviews that share date, tail num, origin, dest, and departure time
+# Note that the entire row is the value here
+all_dupes = airport.map(lambda x: ((x[3], x[6], x[11], x[15], x[16]), x))
+# Group by row values, dropping duplicates
+airport = all_dupes.reduceByKey(lambda x, y: x) \
+                   .map(lambda x: x[1])
 
-# x[12] is destination
-# x[16] is arrival time
+# x11 is ORIGIN
+# x15 is DEST
+# filter: select airport of interest
+# map: emit: (year, (airport of interest?, airport of interest?))
+# reduceByKey: count arrivals and departures by year
+# map: flatten the keys and values
 
-#get count of departures for every unique pair (day_of_month, hour) in the month
-deps_per_hour = airport.filter(lambda x: x[12] == ap_select)  \
-               .map(lambda x: ((x[0], x[1], x[2], (extract_hour(x[16]))), 1)) \
-               .reduceByKey(lambda x, y: x+y) 
+rdd = airport.filter(lambda x: x[8] == ap_select or x[12] == ap_select) \
+             .map(lambda x: (x[0], (is_selected(x[8]), is_selected(x[12])))) \
+             .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])) \
+             .map(lambda x: x[:-1] + x[-1])
 
-# join on the complex keys
-hours = arrs_per_hour.join(deps_per_hour) \
-	             .map(lambda x: (x[0][0], x[0][1], x[0][2], x[0][3], x[1][0], x[1][1]))
-
-#rdd = airport.map(lambda x:(x[0], 1)) \
-#             .reduceByKey(lambda x, y: x+y)
+rdd = airport.map(lambda x:(x[0], 1)) \
+             .reduceByKey(lambda x, y: x+y)
 
 print("Python version: " + platform.python_version())
 #print("RDD count: " + str(rdd.count()))
 
-print(hours.take(15))
+print(rdd.take(15))
 
 # collect and save as csv
 #with open('/Akamai_scratch/airport_alpha/test_csv_stl.csv', 'wb') as myfile:
@@ -114,7 +99,6 @@ print(hours.take(15))
 #spark = SparkSession.builder.config(conf=conf).getOrCreate() 
 
 
-df = sess.createDataFrame(hours, ['year','month', 'day_of_month', 'hour', 'arrivals', 'departures'])
-#df = sess.createDataFrame(rdd, ['year', 'count'])
-df.coalesce(1).write.save(path='./test_csv_atl.csv', format='csv', mode='append')
-
+#df = sess.createDataFrame(rdd, ['year', 'arrivals', 'departures'])
+df = sess.createDataFrame(rdd, ['year', 'count'])
+df.coalesce(1).write.save(path='./test_csv_stl.csv', format='csv', mode='append')
